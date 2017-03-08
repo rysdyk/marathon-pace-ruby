a10f7d1d23959fbbb95eb3b1fa95b5d592112ded
Samuel Rysdyk <samuel.rysdyk@gmail.com>
2017-03-08 09:09:26 -0600

    work on mile pace and any pace functions

--- marathon_analysis.rb
+++ marathon_analysis.rb
@@ -1,5 +1,12 @@
 require 'time'
 
+# 'would you like to enter mile pace or finish time?'
+# get that time
+# if marathon time, here are your splits and goal estimates
+# if mile pace, here is your finish time and goal estimates
+
+# irb -r ./marathon_analysis.rb
+
 #get mile avg based on final time.
 def average_mile(finish_time)
   t = Time.parse(finish_time)
@@ -37,22 +44,53 @@ def splits(marathon_time)
   end
 end
 
-def mile_pace(mile_time)
-  # convert minutes into seconds
-  t = Time.parse(mile_time)
-  seconds = t.min * 60 + t.sec
-  puts seconds
-
-  # splits(marathon_time)
-end
-
+# takes finish time and prints out splits and average mile pace
 def finish_time(result)
   splits(result)
   average = average_mile(result)
   puts "Average mile time: #{average}"
 end
 
+# takes mile pace and figures out total marathon finish time
+def mile_pace(mile_time)
+  # requires hour in mile time
+  t = Time.parse(mile_time)
+  # convert minutes into seconds
+  mile_seconds = t.min * 60 + t.sec
+  total_seconds = mile_seconds * 26.2
+  total_minutes = total_seconds / 60
+  hours = (total_minutes / 60).floor
+  minutes = (total_minutes % 60).floor
+  seconds = total_minutes % 1
+  seconds = (60 * seconds).round
+  # the fast way to do all of that
+  # time = Time.at(total_seconds).utc.strftime("%H:%M:%S")
+  time = "#{sprintf("%02d", hours)}:#{sprintf("%02d", minutes)}:#{sprintf("%02d", seconds)}"
+  return time
+  # splits(marathon_time)
+end
+
+# takes total time for any event and returns mile pace 
+def pace(finish_time, distance)
+  # takes finish_time of any distance and returns out mile pace
+  t = Time.parse(finish_time)
+  total_seconds = t.hour * 3600 + t.min * 60 + t.sec
+  seconds = total_seconds / distance
+  mile_minutes = (seconds / 60).floor
+  mile_seconds = (seconds / 60) % 1
+  mile_seconds = 60 * mile_seconds
+  pace = "#{sprintf("%02d", mile_minutes)}:#{sprintf("%02d", mile_seconds.round)}"
+  # if distance is array, returns out splits
+end
+
 #def goals
   # how much faster to mile to reach next round number?
   # how much faster per mile to finish 10 minutes faster
+    # hint: 23 seconds per mile to finish 10 minutes faster
 #end
+
+# add raise exceptions if numbers not right...
+
+# note that these are estimates since half a second per mile over a marathon is 13 seconds.
+
+# realistic split difference aka mile 1 will be faster than mile 26
--- tests/marathon_test.rb
+++ tests/marathon_test.rb
@@ -1,3 +1,4 @@
+# ruby tests/marathon_test.rb
 
 require 'minitest/autorun'
 require './marathon_analysis.rb'
@@ -7,5 +8,18 @@ class MileTest < MiniTest::Test
   def test_mile_time
     assert_equal "09:09", average_mile("4:00")
     assert_equal "09:09", average_mile("4:00:00")
+  end
+
+  def test_decimal_to_seconds
+    assert_equal "08:30", decimal_to_seconds(8.5)
+    assert_equal "08:15", decimal_to_seconds(8.25)
+  end
+
+  def test_mile_pace_to_finish_time
+    assert_equal "03:59:44", mile_pace("0:09:09")
+  end
+
+  def test_pace
+    assert_equal "08:01", pace("01:45:00", 13.1)
   end
 end
